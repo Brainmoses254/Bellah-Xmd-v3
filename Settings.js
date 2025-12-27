9e8a1e6329529dcba4a1798d92c967080130d38d
Brainmoses254 <joinmoses254@gmail.com>
2025-12-28 00:13:00 +0300

    Create Settings.js

---
+++ Settings.js
@@ -0,0 +1,60 @@
+
+const fs = require('fs')
+const chalk = require('chalk')
+if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
+
+
+global.SESSION_ID = process.env.SESSION_ID || 'Bellah~m2xDSJAS#cXaReUMFXZBxDu0kZ8ck_cgjIEBgC2_-AUY-3go8qwM' 
+
+global.ytname = "YT: GIDDYTENNOR" 
+global.socialm = "IG: @GIDDYTENNOR" 
+global.location = "Kenya" 
+
+//new
+global.botname = process.env.BOT_NAME ||'𝐁𝐞𝐥𝐥𝐚𝐡 𝐗𝐦𝐝' //enter your  bot name here
+global.ownernumber = process.env.OWNER_NUMBER ||'254710329917' //ur owner number
+global.ownername = 'Giddy Tennor' //ur owner name
+global.websitex = "https://chat.whatsapp.com/DkaqpXIK87HE11pAHWERN8" 
+global.wagc = "https://chat.whatsapp.com/DkaqpXIK87HE11pAHWERN8" 
+global.themeemoji = '🪀'
+global.wm = "GIDDY TENNOR"
+global.botscript = 'https://chat.whatsapp.com/DkaqpXIK87HE11pAHWERN8' 
+global.packname = process.env.PACK_NAME ||"VolTah Xmd" //enter your stickers author name here
+global.author = "Giddy-Tennor"
+global.creator = "254710329917@s.whatsapp.net"
+global.xprefix = process.env.BOT_PREFIX ||'.'
+global.hituet = 0
+
+//bot settings 
+global.autoblocknumber = process.env.AUTOBLOCK_NUMBER || '263,234' //set autoblock country code
+global.antiforeignnumber = process.env.ANTIFOREIGN_NUMBER || '' //set anti foreign number country code
+global.mode = process.env.MODE || 'public' //set bot public/private
+const antilinkgc = process.env.ANTILINK_GC || 'TRUE';
+global.anticall = process.env.ANTI_CALL || 'false' //bot blocks user when called
+global.autostatusview = process.env.AUTOSW_VIEW || 'true' //auto status/story view
+global.adminevent = true //show promote/demote message
+global.groupevent = process.env.GROUP_EVENT || 'false' //show update messages in group chat
+//msg
+const appname = process.env.APP_NAME || '';
+const herokuapi = process.env.HEROKU_API;
+
+global.mess = {
+        limit: 'Your limit is up <\>',
+        nsfw: 'Nsfw is disabled in this group, Please tell the admin to enable',
+        owner: 'Bellah Xmd owner only<\>',
+    admin: 'Bot is not admin<\>',
+    group: 'feature for group only<\>',
+    done: 'Done ✓',
+    error: 'Error !',
+    success: 'Succes •'
+}
+//thumbnail
+
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+    fs.unwatchFile(file)
+    console.log(chalk.redBright(`Update'${__filename}'`))
+    delete require.cache[file]
+    require(file)
+})
